dcd3f4fce39f876111c2d620ea945ec033abc33a
maksim butakov <qulifis@gmail.com>
2023-11-03 01:02:50 +0600

    Improve style

--- src/main/java/com/biscof/urlshortener/AppExceptionHandler.java
+++ src/main/java/com/biscof/urlshortener/AppExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class AppExceptionHandler {
 
-    @ExceptionHandler({ LinkNotFoundException.class })
+    @ExceptionHandler(LinkNotFoundException.class)
     public ResponseEntity<Object> handleLinkNotFoundException(LinkNotFoundException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
--- src/main/java/com/biscof/urlshortener/exception/LinkNotFoundException.java
+++ src/main/java/com/biscof/urlshortener/exception/LinkNotFoundException.java
@@ -3,7 +3,7 @@ package com.biscof.urlshortener.exception;
 public class LinkNotFoundException extends RuntimeException {
 
     public LinkNotFoundException(String shortUrl) {
-        super(String.format("URL %s not found.", shortUrl));
+        super(String.format("URL \"%s\" not found.", shortUrl));
     }
 
 }
